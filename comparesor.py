66411d4dd4282870a22b09269490fd98f518ad8a
Fenrir <tomasz.litwicki@gmail.com>
2026-07-15 12:43:27 +0200

    colors

--- comparesor.py
+++ comparesor.py
@@ -8,6 +8,10 @@ NAME_FILE_2 = '2'
 ### RESZTA KODU DZIAŁA ODTĄD SAMA ###
 import json
 
+RED = "\033[1;4;31m"
+GREEN = "\033[1;4;32m"
+UNDERLINE = "\033[4m"
+RESET = "\033[0m"
 founded_a = []
 founded_b = []
 not_founded_a: list = SEARCH_LIST.copy()
@@ -20,12 +24,12 @@ with open(f'{NAME_FILE_2}.json') as b_json:
     b = json.load(b_json)['Characteristic']
 
 if a == b:
-    print("Both files are the same :)")
+    print(f"{GREEN}Both files are the same :){RESET}")
     exit()
 else:
-    print("Nope! Something is wrong and the files aren't precisely identical. \n")
+    print(f"{RED}Nope! Something is wrong and the files aren't precisely identical.{RESET} \n")
 
-print("COMPARE WITH YOUR SEARCHING LIST:\n")
+print(f"{UNDERLINE}COMPARE WITH YOUR SEARCHING LIST:{RESET}\n")
 ### JSON A ###
 print(f"WHAT EXIST IN JSON {NAME_FILE_1}:")
 for i in a:
@@ -48,11 +52,12 @@ print(f'\nNOT FOUNDED IN {NAME_FILE_2} JSON: {not_founded_b}')
 
 print()
 if not_founded_a == not_founded_b:
-    print("In both json's files not founded the same keys.")
+    print(f"{GREEN}In both json's files not founded the same keys.{RESET}")
 else:
-    print("The json's files have a different 'not_founded' keys.\n")
+    print(f"{RED}The json's files have a different 'not_founded' keys.{RESET}\n")
 
 ### COMPARE DIFFERENCE ###
+print(f"{UNDERLINE}COMPARE VALUES IN FOUNDED SEARCHED KEYS:{RESET}\n")
 in_a_not_in_b = list(set(founded_a) - set(founded_b))
 print(f"IN {NAME_FILE_1} NOT IN {NAME_FILE_2}: {in_a_not_in_b}")
 in_b_not_in_a = list(set(founded_b) - set(founded_a))
@@ -68,7 +73,7 @@ for i in in_a_and_in_b:
     print(f"JSON {NAME_FILE_1}: {[ value_from_a := j['Value'] for j in a if j['Name'] == i]}")
     print(f"JSON {NAME_FILE_2}: {[ value_from_b := j['Value'] for j in b if j['Name'] == i]}")
     if value_from_a == value_from_b:
-        print("THE SAME :)")
+        print(f"{GREEN}THE SAME :){RESET}")
     else:
-        print("NO!!! Not the same :(")
+        print(f"{RED}NO!!! Not the same :({RESET}")
     print()
